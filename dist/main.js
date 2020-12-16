68a551e65c8d739fbb3cde89c5650668d2abb6c8
ABHIGYAN MAHANTA <abhigyanmahanta001@gmail.com>
2020-12-16 20:17:38 +0530

    Add API integration

--- dist/main.js
+++ dist/main.js
@@ -10,6 +10,6 @@
 /*!**********************!*\
   !*** ./src/index.js ***!
   \**********************/
-eval("\n\n//# sourceURL=webpack:///./src/index.js?");
+eval("const api = {\r\n  key: \"4cc8a926413ba21a3774022235df5269\",\r\n  base: \"https://api.openweathermap.org/data/2.5/\"\r\n}\r\n\r\nconst searchbox = document.querySelector('.search-box');\r\nsearchbox.addEventListener('keypress', setQuery);\r\n\r\nfunction setQuery(evt) {\r\n  if (evt.keyCode == 13) {\r\n    getResults(searchbox.value);\r\n  }\r\n}\r\n\r\nfunction getResults (query) {\r\n  fetch(`${api.base}weather?q=${query}&units=metric&APPID=${api.key}`)\r\n    .then(weather => {\r\n      return weather.json();\r\n    }).then(displayResults);\r\n}\n\n//# sourceURL=webpack:///./src/index.js?");
 /******/ })()
 ;
---
+++ src/index.js
@@ -0,0 +1,20 @@
+const api = {
+  key: "4cc8a926413ba21a3774022235df5269",
+  base: "https://api.openweathermap.org/data/2.5/"
+}
+
+const searchbox = document.querySelector('.search-box');
+searchbox.addEventListener('keypress', setQuery);
+
+function setQuery(evt) {
+  if (evt.keyCode == 13) {
+    getResults(searchbox.value);
+  }
+}
+
+function getResults (query) {
+  fetch(`${api.base}weather?q=${query}&units=metric&APPID=${api.key}`)
+    .then(weather => {
+      return weather.json();
+    }).then(displayResults);
+}
