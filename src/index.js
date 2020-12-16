9b83b3705666b6af53744adfbdaf4c497b1b127c
ABHIGYAN MAHANTA <abhigyanmahanta001@gmail.com>
2020-12-16 22:42:54 +0530

    Fix linter issues and add favicon

--- src/index.js
+++ src/index.js
@@ -1,48 +1,47 @@
+/* eslint-disable no-use-before-define */
 const api = {
-  key: "4cc8a926413ba21a3774022235df5269",
-  base: "https://api.openweathermap.org/data/2.5/"
-}
+  key: '4cc8a926413ba21a3774022235df5269',
+  base: 'https://api.openweathermap.org/data/2.5/',
+};
 
 const searchbox = document.querySelector('.search-box');
 searchbox.addEventListener('keypress', setQuery);
 
 function setQuery(evt) {
-  if (evt.keyCode == 13) {
+  if (evt.keyCode === 13) {
     getResults(searchbox.value);
   }
 }
 
-function getResults (query) {
-  fetch(`${api.base}weather?q=${query}&units=metric&APPID=${api.key}`)
-    .then(weather => {
-      return weather.json();
-    }).then(displayResults);
+async function getResults(query) {
+  await fetch(`${api.base}weather?q=${query}&units=metric&APPID=${api.key}`)
+    .then(weather => weather.json()).then(displayResults);
 }
 
-function displayResults (weather) {
-  let city = document.querySelector('.location .city');
+function displayResults(weather) {
+  const city = document.querySelector('.location .city');
   city.innerText = `${weather.name}, ${weather.sys.country}`;
 
-  let now = new Date();
-  let date = document.querySelector('.location .date');
+  const now = new Date();
+  const date = document.querySelector('.location .date');
   date.innerText = dateBuilder(now);
 
-  let temp = document.querySelector('.current .temp');
+  const temp = document.querySelector('.current .temp');
   temp.innerHTML = `${Math.round(weather.main.temp)}<span>°C</span>`;
 
-  let weather_el = document.querySelector('.current .weather');
-  weather_el.innerText = weather.weather[0].main;
+  const weatherEl = document.querySelector('.current .weather');
+  weatherEl.innerText = weather.weather[0].main;
 
-  let hilow = document.querySelector('.hi-low');
+  const hilow = document.querySelector('.hi-low');
   hilow.innerText = `${Math.round(weather.main.temp_min)}°C / ${Math.round(weather.main.temp_max)}°C`;
 }
 
-function dateBuilder (d) {
-  const months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
-  const days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
-  let day = days[d.getDay()];
-  let date = d.getDate();
-  let month = months[d.getMonth()];
-  let year = d.getFullYear();
+function dateBuilder(d) {
+  const months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
+  const days = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
+  const day = days[d.getDay()];
+  const date = d.getDate();
+  const month = months[d.getMonth()];
+  const year = d.getFullYear();
   return `${day} ${date} ${month} ${year}`;
 }
