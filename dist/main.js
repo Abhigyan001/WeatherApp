5b6e074ef9a872c7ba1b17a87a85beed6d716802
ABHIGYAN MAHANTA <abhigyanmahanta001@gmail.com>
2020-12-17 13:11:33 +0530

    Implement modules for index.js

--- dist/main.js
+++ dist/main.js
@@ -7,9 +7,97 @@
  * If you are looking for production-ready output files, see mode: "production" (https://webpack.js.org/configuration/mode/).
  */
 /******/ (() => { // webpackBootstrap
+/******/ 	"use strict";
+/******/ 	var __webpack_modules__ = ({
+
+/***/ "./src/index.js":
 /*!**********************!*\
   !*** ./src/index.js ***!
   \**********************/
-eval("/* eslint-disable no-use-before-define */\nconst api = {\n  key: '4cc8a926413ba21a3774022235df5269',\n  base: 'https://api.openweathermap.org/data/2.5/',\n};\n\nconst searchbox = document.querySelector('.search-box');\nsearchbox.addEventListener('keypress', setQuery);\n\nfunction setQuery(evt) {\n  if (evt.keyCode === 13) {\n    getResults(searchbox.value);\n  }\n}\n\nasync function getResults(query) {\n  await fetch(`${api.base}weather?q=${query}&units=metric&APPID=${api.key}`)\n    .then(weather => weather.json()).then(displayResults);\n}\n\nfunction displayResults(weather) {\n  const city = document.querySelector('.location .city');\n  city.innerText = `${weather.name}, ${weather.sys.country}`;\n\n  const now = new Date();\n  const date = document.querySelector('.location .date');\n  date.innerText = dateBuilder(now);\n\n  const temp = document.querySelector('.current .temp');\n  temp.innerHTML = `${Math.round(weather.main.temp)}<span>°C</span>`;\n\n  const weatherEl = document.querySelector('.current .weather');\n  weatherEl.innerText = weather.weather[0].main;\n\n  const hilow = document.querySelector('.hi-low');\n  hilow.innerText = `${Math.round(weather.main.temp_min)}°C / ${Math.round(weather.main.temp_max)}°C`;\n}\n\nfunction dateBuilder(d) {\n  const months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];\n  const days = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];\n  const day = days[d.getDay()];\n  const date = d.getDate();\n  const month = months[d.getMonth()];\n  const year = d.getFullYear();\n  return `${day} ${date} ${month} ${year}`;\n}\n\n//# sourceURL=webpack:///./src/index.js?");
+/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
+
+eval("__webpack_require__.r(__webpack_exports__);\n/* harmony import */ var _modules_results__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! ./modules/results */ \"./src/modules/results.js\");\n\r\n\r\n(0,_modules_results__WEBPACK_IMPORTED_MODULE_0__.setQuery)(13);\n\n//# sourceURL=webpack://weatherapp/./src/index.js?");
+
+/***/ }),
+
+/***/ "./src/modules/results.js":
+/*!********************************!*\
+  !*** ./src/modules/results.js ***!
+  \********************************/
+/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
+
+eval("__webpack_require__.r(__webpack_exports__);\n/* harmony export */ __webpack_require__.d(__webpack_exports__, {\n/* harmony export */   \"setQuery\": () => /* binding */ setQuery\n/* harmony export */ });\n/* harmony import */ var _weather__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! ./weather */ \"./src/modules/weather.js\");\n\r\nconst api = {\r\n  key: '4cc8a926413ba21a3774022235df5269',\r\n  base: 'https://api.openweathermap.org/data/2.5/',\r\n};\r\n\r\nconst searchbox = document.querySelector('.search-box');\r\nsearchbox.addEventListener('keypress', setQuery);\r\n\r\nfunction setQuery(evt) {\r\n  if (evt.keyCode === 13) {\r\n    getResults(searchbox.value);\r\n  }\r\n}\r\n\r\nasync function getResults(query) {\r\n  await fetch(`${api.base}weather?q=${query}&units=metric&APPID=${api.key}`)\r\n    .then(weather => weather.json()).then(displayResults);\r\n}\r\n\r\nfunction displayResults(weather) {\r\n  const city = document.querySelector('.location .city');\r\n  city.innerText = `${weather.name}, ${weather.sys.country}`;\r\n\r\n  const now = new Date();\r\n  const date = document.querySelector('.location .date');\r\n  date.innerText = (0,_weather__WEBPACK_IMPORTED_MODULE_0__.dateBuilder)(now);\r\n\r\n  const temp = document.querySelector('.current .temp');\r\n  temp.innerHTML = `${Math.round(weather.main.temp)}<span>°C</span>`;\r\n\r\n  const weatherEl = document.querySelector('.current .weather');\r\n  weatherEl.innerText = weather.weather[0].main;\r\n\r\n  const hilow = document.querySelector('.hi-low');\r\n  hilow.innerText = `${Math.round(weather.main.temp_min)}°C / ${Math.round(weather.main.temp_max)}°C`;\r\n}\r\n\r\n\n\n//# sourceURL=webpack://weatherapp/./src/modules/results.js?");
+
+/***/ }),
+
+/***/ "./src/modules/weather.js":
+/*!********************************!*\
+  !*** ./src/modules/weather.js ***!
+  \********************************/
+/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
+
+eval("__webpack_require__.r(__webpack_exports__);\n/* harmony export */ __webpack_require__.d(__webpack_exports__, {\n/* harmony export */   \"dateBuilder\": () => /* binding */ dateBuilder\n/* harmony export */ });\nfunction dateBuilder(d) {\r\n  const months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];\r\n  const days = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];\r\n  const day = days[d.getDay()];\r\n  const date = d.getDate();\r\n  const month = months[d.getMonth()];\r\n  const year = d.getFullYear();\r\n  return `${day} ${date} ${month} ${year}`;\r\n}\n\n//# sourceURL=webpack://weatherapp/./src/modules/weather.js?");
+
+/***/ })
+
+/******/ 	});
+/************************************************************************/
+/******/ 	// The module cache
+/******/ 	var __webpack_module_cache__ = {};
+/******/ 	
+/******/ 	// The require function
+/******/ 	function __webpack_require__(moduleId) {
+/******/ 		// Check if module is in cache
+/******/ 		if(__webpack_module_cache__[moduleId]) {
+/******/ 			return __webpack_module_cache__[moduleId].exports;
+/******/ 		}
+/******/ 		// Create a new module (and put it into the cache)
+/******/ 		var module = __webpack_module_cache__[moduleId] = {
+/******/ 			// no module.id needed
+/******/ 			// no module.loaded needed
+/******/ 			exports: {}
+/******/ 		};
+/******/ 	
+/******/ 		// Execute the module function
+/******/ 		__webpack_modules__[moduleId](module, module.exports, __webpack_require__);
+/******/ 	
+/******/ 		// Return the exports of the module
+/******/ 		return module.exports;
+/******/ 	}
+/******/ 	
+/************************************************************************/
+/******/ 	/* webpack/runtime/define property getters */
+/******/ 	(() => {
+/******/ 		// define getter functions for harmony exports
+/******/ 		__webpack_require__.d = (exports, definition) => {
+/******/ 			for(var key in definition) {
+/******/ 				if(__webpack_require__.o(definition, key) && !__webpack_require__.o(exports, key)) {
+/******/ 					Object.defineProperty(exports, key, { enumerable: true, get: definition[key] });
+/******/ 				}
+/******/ 			}
+/******/ 		};
+/******/ 	})();
+/******/ 	
+/******/ 	/* webpack/runtime/hasOwnProperty shorthand */
+/******/ 	(() => {
+/******/ 		__webpack_require__.o = (obj, prop) => Object.prototype.hasOwnProperty.call(obj, prop)
+/******/ 	})();
+/******/ 	
+/******/ 	/* webpack/runtime/make namespace object */
+/******/ 	(() => {
+/******/ 		// define __esModule on exports
+/******/ 		__webpack_require__.r = (exports) => {
+/******/ 			if(typeof Symbol !== 'undefined' && Symbol.toStringTag) {
+/******/ 				Object.defineProperty(exports, Symbol.toStringTag, { value: 'Module' });
+/******/ 			}
+/******/ 			Object.defineProperty(exports, '__esModule', { value: true });
+/******/ 		};
+/******/ 	})();
+/******/ 	
+/************************************************************************/
+/******/ 	// startup
+/******/ 	// Load entry module
+/******/ 	__webpack_require__("./src/index.js");
+/******/ 	// This entry module used 'exports' so it can't be inlined
 /******/ })()
 ;
---
+++ src/modules/results.js
@@ -0,0 +1,38 @@
+import {dateBuilder} from './weather';
+const api = {
+  key: '4cc8a926413ba21a3774022235df5269',
+  base: 'https://api.openweathermap.org/data/2.5/',
+};
+
+const searchbox = document.querySelector('.search-box');
+searchbox.addEventListener('keypress', setQuery);
+
+export function setQuery(evt) {
+  if (evt.keyCode === 13) {
+    getResults(searchbox.value);
+  }
+}
+
+async function getResults(query) {
+  await fetch(`${api.base}weather?q=${query}&units=metric&APPID=${api.key}`)
+    .then(weather => weather.json()).then(displayResults);
+}
+
+function displayResults(weather) {
+  const city = document.querySelector('.location .city');
+  city.innerText = `${weather.name}, ${weather.sys.country}`;
+
+  const now = new Date();
+  const date = document.querySelector('.location .date');
+  date.innerText = dateBuilder(now);
+
+  const temp = document.querySelector('.current .temp');
+  temp.innerHTML = `${Math.round(weather.main.temp)}<span>°C</span>`;
+
+  const weatherEl = document.querySelector('.current .weather');
+  weatherEl.innerText = weather.weather[0].main;
+
+  const hilow = document.querySelector('.hi-low');
+  hilow.innerText = `${Math.round(weather.main.temp_min)}°C / ${Math.round(weather.main.temp_max)}°C`;
+}
+
